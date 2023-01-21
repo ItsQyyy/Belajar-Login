9fa38df2f78421615fca7f79524df10a6429d097
ItsQyyy <122433554+ItsQyyy@users.noreply.github.com>
2023-01-21 10:56:26 +0700

    Add files via upload

---
+++ Coding/akun.h
@@ -0,0 +1,14 @@
+#include <iostream.h>
+#include <conio.h>
+main();
+akun(char jud[49]="")
+{
+if(strcmp(jud,"akun")==0){
+ cout<<"==========Akun==========="<<endl;
+ cout<<"USERNAME = SMKN1"<<endl;
+ cout<<"PASSWORD = sekolah"<<endl;
+ cout<<"========================="<<endl;
+
+getch();
+}
+}
